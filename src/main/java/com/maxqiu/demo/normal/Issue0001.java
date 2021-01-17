e11a277ac06f97e0785e097ca4e7f2885bd9a6e6
Max-Qiu <qzhing@126.com>
2021-01-17 18:27:31 +0800

    修改为静态方法

--- src/main/java/com/maxqiu/demo/normal/Issue0001.java
+++ src/main/java/com/maxqiu/demo/normal/Issue0001.java
@@ -5,26 +5,26 @@ import java.util.HashMap;
 
 /**
  * 两数之和
- * 
+ *
  * https://leetcode-cn.com/problems/two-sum
- * 
+ *
  * @author Max_Qiu
  */
 public class Issue0001 {
+
     public static void main(String[] args) {
         int[] nums = new int[] {3, 2, 4};
         int target = 6;
-        System.out.println(Arrays.toString(new Solution().twoSum2(nums, target)));
+        System.out.println(Arrays.toString(twoSum1(nums, target)));
+        System.out.println(Arrays.toString(twoSum2(nums, target)));
     }
-}
 
-class Solution {
     /**
      * T：O(N^2)
-     * 
+     *
      * S：O(1)
      */
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum1(int[] nums, int target) {
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -35,7 +35,12 @@ class Solution {
         return null;
     }
 
-    public int[] twoSum2(int[] nums, int target) {
+    /**
+     * T：O(N)
+     *
+     * S：O(N)
+     */
+    public static int[] twoSum2(int[] nums, int target) {
         HashMap<Integer, Integer> hashMap = new HashMap<>(nums.length);
         for (int i = 0; i < nums.length; i++) {
             if (hashMap.containsKey(target - nums[i])) {
@@ -45,4 +50,5 @@ class Solution {
         }
         return null;
     }
+
 }
