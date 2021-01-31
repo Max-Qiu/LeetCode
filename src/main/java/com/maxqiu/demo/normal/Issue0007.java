6e52065a24943ea43f6ee8849c8ed621367022b2
Max-Qiu <qzhing@126.com>
2021-01-31 23:58:00 +0800

    LeetCode 7

---
+++ src/main/java/com/maxqiu/demo/normal/Issue0007.java
@@ -0,0 +1,49 @@
+package com.maxqiu.demo.normal;
+
+/**
+ * 整数反转
+ * 
+ * https://leetcode-cn.com/problems/reverse-integer/
+ * 
+ * @author Max_Qiu
+ */
+public class Issue0007 {
+
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(reverse(123));
+        System.out.println(reverse(-321));
+        System.out.println(reverse(120));
+        System.out.println(reverse(0));
+        System.out.println(reverse(10));
+        System.out.println(reverse(1212121212));
+        System.out.println(reverse(1534236469));
+        System.out.println(reverse(-2147483648));
+    }
+
+    public static int reverse(int x) {
+        if (x == Integer.MIN_VALUE) {
+            return 0;
+        }
+        // 是否为负
+        boolean isMinus = x < 0;
+        if (isMinus) {
+            x = -x;
+        }
+        int i = 0;
+        do {
+            int temp = x % 10;
+            x = x / 10;
+            if ((temp + i * 10) / 10 < i) {
+                return 0;
+            }
+            i = temp + i * 10;
+
+        } while (x > 0);
+        if (isMinus) {
+            return -i;
+        }
+        return i;
+    }
+
+}
