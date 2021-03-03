7a22d6b89465206ca2e5613dc775d311a9921598
Max-Qiu <qzhing@126.com>
2021-03-03 22:45:55 +0800

    LeetCode 83

---
+++ src/main/java/com/maxqiu/demo/normal/Issue0083.java
@@ -0,0 +1,69 @@
+package com.maxqiu.demo.normal;
+
+/**
+ * 删除排序链表中的重复元素
+ * 
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+ * 
+ * @author Max_Qiu
+ */
+public class Issue0083 {
+
+    public static void main(String[] args) {
+        System.out.println(deleteDuplicates(new ListNode(1)));
+        System.out.println(deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2)))));
+        System.out.println(deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(1)))));
+        System.out.println(
+            deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))))));
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        // 如果当前节点为空，则返回null
+        if (head == null) {
+            return null;
+        }
+        // 如果当前节点的下一个值为空，则返回当前节点
+        if (head.next == null) {
+            return head;
+        }
+        // 如果当前节点的值和下个节点的值相同
+        if (head.val == head.next.val) {
+            // 则当前节点等于下个节点的递归
+            head = deleteDuplicates(head.next);
+        } else {
+            // 否则下个节点等于下个节点的递归
+            head.next = deleteDuplicates(head.next);
+        }
+        return head;
+    }
+
+    /**
+     * 单向链表节点
+     */
+    static class ListNode {
+
+        int val;
+
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            String s = val + "";
+            if (this.next != null) {
+                s += "," + this.next.toString();
+            }
+            return s;
+        }
+
+    }
+
+}
