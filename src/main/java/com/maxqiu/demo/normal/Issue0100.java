62a05a4b37c38a13d49b0e51cae08752d056f7b4
Max-Qiu <qzhing@126.com>
2021-03-05 07:41:00 +0800

    LeetCode 100

---
+++ src/main/java/com/maxqiu/demo/normal/Issue0100.java
@@ -0,0 +1,48 @@
+package com.maxqiu.demo.normal;
+
+/**
+ * 相同的树
+ * 
+ * https://leetcode-cn.com/problems/same-tree/
+ * 
+ * @author Max_Qiu
+ */
+public class Issue0100 {
+
+    public static void main(String[] args) {
+        System.out.println(isSameTree(new TreeNode(1, new TreeNode(2), new TreeNode(3)),
+            new TreeNode(1, new TreeNode(2), new TreeNode(3))));
+
+        System.out.println(isSameTree(new TreeNode(1, new TreeNode(2), null), new TreeNode(1, null, new TreeNode(3))));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p != null && q != null) {
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+        return false;
+    }
+
+    /**
+     * 二叉树节点
+     */
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
