66bce2e1097e65a30cbf27215ae7ea96405ee6e7
Max-Qiu <qzhing@126.com>
2021-05-06 22:17:34 +0800

    LeetCode 257

---
+++ src/main/java/com/maxqiu/demo/normal/Issue0257.java
@@ -0,0 +1,42 @@
+package com.maxqiu.demo.normal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.maxqiu.demo.entity.TreeNode;
+
+/**
+ * @author Max_Qiu
+ */
+public class Issue0257 {
+    public static void main(String[] args) {
+        System.out.println(binaryTreePaths(new TreeNode(1, new TreeNode(2, null, new TreeNode(5)), new TreeNode(3))));
+    }
+
+    public static List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        String parent = root.val + "";
+        list.addAll(append(root, parent));
+        return list;
+    }
+
+    public static List<String> append(TreeNode node, String parent) {
+        if (node.left == null && node.right == null) {
+            ArrayList<String> list = new ArrayList<>();
+            list.add(parent);
+            return list;
+        }
+        if (node.left != null && node.right == null) {
+            return append(node.left, parent + "->" + node.left.val);
+        }
+        if (node.left == null) {
+            return append(node.right, parent + "->" + node.right.val);
+        }
+        List<String> list = append(node.left, parent + "->" + node.left.val);
+        list.addAll(append(node.right, parent + "->" + node.right.val));
+        return list;
+    }
+}
