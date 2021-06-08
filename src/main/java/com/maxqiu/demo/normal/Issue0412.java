9ef8e362bfb1f7729b785cecfbc3567d0542510b
Max-Qiu <qzhing@126.com>
2021-06-08 23:30:14 +0800

    LeetCode 412

---
+++ src/main/java/com/maxqiu/demo/normal/Issue0412.java
@@ -0,0 +1,31 @@
+package com.maxqiu.demo.normal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Max_Qiu
+ */
+public class Issue0412 {
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(15));
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            String s = "";
+            if (i % 3 == 0) {
+                s += "Fizz";
+            }
+            if (i % 5 == 0) {
+                s += "Buzz";
+            }
+            if (s.length() == 0) {
+                s = i + "";
+            }
+            list.add(s);
+        }
+        return list;
+    }
+}
