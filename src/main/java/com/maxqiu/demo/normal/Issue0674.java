4d0167eaaf955c2415d764c0dba873e5651bb963
Max-Qiu <qzhing@126.com>
2021-11-18 22:04:19 +0800

    LeetCode674

---
+++ src/main/java/com/maxqiu/demo/normal/Issue0674.java
@@ -0,0 +1,25 @@
+package com.maxqiu.demo.normal;
+
+/**
+ * @author Max_Qiu
+ */
+public class Issue0674 {
+    public static void main(String[] args) {
+        System.out.println(findLengthOfLCIS(new int[] {1, 3, 5, 4, 7}));
+        System.out.println(findLengthOfLCIS(new int[] {2, 2, 2, 2, 2}));
+        System.out.println(findLengthOfLCIS(new int[] {1, 3, 5, 7}));
+    }
+
+    public static int findLengthOfLCIS(int[] nums) {
+        int max = 1, count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
